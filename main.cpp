e9be56e641fdbe30b0091e013b2848eab487bc80
Evgeniy <e.koskov@bk.ru>
2025-02-09 16:19:30 +0500

    second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <locale>
+#include <windows.h>
+#include <string>
+#include <cstdint>
 
 using namespace std;
 
+#pragma pack(push, 1)
+
+struct Chunk
+{
+    uint32_t Length;
+    char Type[4];
+    vector<char> Data;
+    uint32_t CRC;
+};
+
+#pragma pack(pop)
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL, "Russian");
+
     return 0;
 }
