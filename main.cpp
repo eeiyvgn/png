e96a2e42d8013a49446e12b0864dcbdb9ad87670
Evgeniy <e.koskov@bk.ru>
2025-02-09 16:35:23 +0500

    third commit

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@ struct Chunk
 {
     uint32_t Length;
     char Type[4];
-    vector<char> Data;
     uint32_t CRC;
 };
 
@@ -23,6 +22,20 @@ struct Chunk
 int main()
 {
     setlocale(LC_ALL, "Russian");
+
+    ifstream png_file("Opera-wide.png", ios::binary);
+
+    if (png_file.is_open())
+    {
+        png_file.seekg(0, ios::end);
+        int fileSize = png_file.tellg();
+        png_file.seekg(0, ios::beg);
+
+        vector<char> png_data(fileSize, 0);
+        png_file.read(png_data.data(), fileSize);
+
+        size_t offset = 8;
+    }
 
     return 0;
 }
